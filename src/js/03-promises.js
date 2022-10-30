6e44ae0f9e86f0b848a4c65b173dd168eb333132
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-10-30 17:22:03 +0200

    add third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  formRef: document.querySelector('.form'),
+};
+
+refs.formRef.addEventListener('submit', onBtnClick);
+
+function onBtnClick(evt) {
+  evt.preventDefault();
+
+  let firstDelay = Number(refs.formRef.delay.value);
+  const delayStep = Number(refs.formRef.step.value);
+  const promisesAmount = Number(refs.formRef.amount.value);
+
+  for (let i = 1; i <= promisesAmount; i += 1) {
+    createPromise(i, firstDelay);
+    firstDelay += delayStep;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value => {
+      console.log(value);
+      Notiflix.Notify.success(value);
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure(error);
+    });
 }
